f8d8fddf7269334e0cc8888bfbd66bac3a221395
V-Gnana-Teja <151600407+V-Gnana-Teja@users.noreply.github.com>
2024-03-30 22:01:05 +0530

    completed json files to import images

--- src/components/NewsCarousel.jsx
+++ src/components/NewsCarousel.jsx
@@ -1,30 +1,27 @@
 import React from "react";
-import Banner from "../assets/images/Banner (2).png";
-import Img1 from "../assets/images/Rectangle 24 (2).png";
-import Img2 from "../assets/images/Rectangle 24 (3).png";
-import Img3 from "../assets/images/Rectangle 24 (4).png";
-import Img4 from "../assets/images/Rectangle 24 (5).png";
-import Img5 from "../assets/images/Rectangle 24 (6).png";
-import Img6 from "../assets/images/Rectangle 24 (7).png";
-import Img7 from "../assets/images/Rectangle 24 (8).png";
-import Img8 from "../assets/images/Rectangle 24 (9).png";
-import Img9 from "../assets/images/Rectangle 24 (10).png";
+import imagesData from "../news.json";
 
 const NewsCarousel = () => {
+  const { banner, images } = imagesData;
+
   return (
     <div>
       <div>
-        <img className="w-full" src={Banner} alt="/" />
+
+        <img className="w-full" src={banner} alt="/" />
       </div>
 
-      <div className=" flex flex-col items-center justify-center mt-12">
-        <h1 className=" text-3xl">Travelaja Articles</h1>
+      <div className="flex flex-col items-center justify-center mt-12">
+
+        <h1 className="text-3xl">Travelaja Articles</h1>
+
         <p className="text-lg mt-2">
           Lorem ipsum dolor sit amet, consectetur adipiscing elit ut aliquam
         </p>
       </div>
 
       <div className="mt-4 flex justify-center p-14">
+
         <button className="text-black hover:bg-green-700 hover:text-white font-bold py-2 px-4 mr-2 rounded">
           Adventure Travel
         </button>
@@ -38,116 +35,30 @@ const NewsCarousel = () => {
           Family Holidays
         </button>
         <button className="text-black hover:bg-green-700 hover:text-white font-bold py-2 px-4 rounded">
-          Art and culture
+          Art and Culture
         </button>
+
       </div>
 
+
       <div className="mt-12 grid grid-cols-3 gap-4 pl-5 ">
-        <div className="max-w-sm rounded-md  overflow-hidden shadow-2xl cursor-pointer">
-          <img src={Img1} alt="/" className="w-full  border-0 border-white" />
-          <div className="px-6 py-4">
-            <div className=" text-xl mb-2">
-              where can i go? 5 amazing countries that are open right now
-            </div>
-            <div className="flex items-center justify-between">
-              <p>September 19, 2024</p>
-            </div>
-          </div>
-        </div>
+        {images.map((image) => (
+          <div
+            key={image.id}
+            className="max-w-sm rounded-md overflow-hidden shadow-2xl cursor-pointer"
+          >
 
-        <div className="max-w-sm rounded-md overflow-hidden shadow-2xl cursor-pointer">
-          <img src={Img2} alt="/" className="w-full border-0 border-white" />
-          <div className="px-6 py-4">
-            <div className=" text-xl mb-2">
-              beautiful view of indonesia's natural hills
-            </div>
-            <div className="flex items-center justify-between">
-              <p>September 19, 2024</p>
-            </div>
-          </div>
-        </div>
+            <img src={image.path} alt={image.altText} className="w-full border-0 border-white" />
+            <div className="px-6 py-4">
 
-        <div className="max-w-sm rounded-md overflow-hidden shadow-2xl cursor-pointer">
-          <img src={Img3} alt="/" className="w-full border-0 border-white" />
-          <div className="px-6 py-4">
-            <div className=" text-xl mb-2">
-              10 Indonesian Destinations you should visit in this year
-            </div>
-            <div className="flex items-center justify-between">
-              <p>September 19, 2024</p>
-            </div>
-          </div>
-        </div>
+              <div className="text-xl mb-2">{image.title}</div>
 
-        <div className="max-w-sm rounded-md overflow-hidden shadow-2xl cursor-pointer">
-          <img src={Img4} alt="/" className="w-full border-0 border-white" />
-          <div className="px-6 py-4">
-            <div className=" text-xl mb-2">
-              5 mountains that you must visit while in Indonesia
-            </div>
-            <div className="flex items-center justify-between">
-              <p>September 19, 2024</p>
+              <div className="flex items-center justify-between">
+                <p>September 19, 2024</p>
+              </div>
             </div>
           </div>
-        </div>
-
-        <div className="max-w-sm rounded-md overflow-hidden shadow-2xl cursor-pointer">
-          <img src={Img5} alt="/" className="w-full border-0 border-white" />
-          <div className="px-6 py-4">
-            <div className=" text-xl mb-2">
-              the most interesting historical monuments in Indonesia
-            </div>
-            <div className="flex items-center justify-between">
-              <p>September 19, 2024</p>
-            </div>
-          </div>
-        </div>
-
-        <div className="max-w-sm rounded-md overflow-hidden shadow-2xl cursor-pointer">
-          <img src={Img6} alt="/" className="w-full  border-0 border-white" />
-          <div className="px-6 py-4">
-            <div className=" text-xl mb-2">
-              travel booking before missing the discount
-            </div>
-            <div className="flex items-center justify-between">
-              <p>September 19, 2024</p>
-            </div>
-          </div>
-        </div>
-
-        <div className="max-w-sm rounded-md overflow-hidden shadow-2xl cursor-pointer">
-          <img src={Img7} alt="/" className="w-full border-0 border-white" />
-          <div className="px-6 py-4">
-            <div className=" text-xl mb-2">
-              10 Indonesian Destinations you should visit in this year
-            </div>
-            <div className="flex items-center justify-between">
-              <p>September 19, 2024</p>
-            </div>
-          </div>
-        </div>
-        <div className="max-w-sm rounded-md overflow-hidden shadow-2xl cursor-pointer">
-          <img src={Img8} alt="/" className="w-full border-0 border-white" />
-          <div className="px-6 py-4">
-            <div className=" text-xl mb-2">
-              travel booking before missing the discount
-            </div>
-            <div className="flex items-center justify-between">
-              <p>September 19, 2024</p>
-            </div>
-          </div>
-        </div>
-        <div className="max-w-sm rounded-md overflow-hidden shadow-2xl cursor-pointer">
-          <img src={Img9} alt="/" className="w-full border-0 border-white" />
-          <div className="px-6 py-4">
-            <div className=" text-xl mb-2">
-              where can i go? 5 amazing countries that are open right now
-            </div>
-            <div className="flex items-center justify-between">
-              <p>September 19, 2022</p>
-            </div>
-          </div>
-        </div>
+        ))}
       </div>
     </div>
   );
--- src/components/ServiceHero.jsx
+++ src/components/ServiceHero.jsx
@@ -1,191 +1,146 @@
 import React from "react";
-import Banner from "../assets/images/Banner (1).png";
 import { ImPriceTags } from "react-icons/im";
 import { RiTreasureMapFill } from "react-icons/ri";
 import { TfiMapAlt } from "react-icons/tfi";
 import { MdOutlinePhotoCamera } from "react-icons/md";
-import Img1 from "../assets/images/Rectangle 21.png";
-import Img2 from "../assets/images/Rectangle 21 (1).png";
-import Img3 from "../assets/images/Rectangle 21 (2).png";
-import Img4 from "../assets/images/Rectangle 22 (1).png";
-import { FaBabyCarriage } from "react-icons/fa6";
+import { FaBabyCarriage, FaCampground } from "react-icons/fa6";
 import { PiWineFill } from "react-icons/pi";
-import { FaCampground } from "react-icons/fa6";
 import { LiaMapMarkedAltSolid } from "react-icons/lia";
+import serviceData from "../services.json";
 
 const ServiceHero = () => {
+  const {
+    bannerImage, services, featuredServices, facilities, additionalServices,
+ 
+  } = serviceData;
+
   return (
     <div>
+
       <div>
-        <img className="w-full" src={Banner} alt="/" />
+        <img className="w-full" src={bannerImage} alt="/" />
       </div>
+
       <div className="flex flex-row items-center justify-between p-12 space-y-4">
-        <div className="bg-white shadow-md p-4 rounded-lg">
-          <div>
-            <ImPriceTags className="w-[40px] h-[40px]" />
-          </div>
-          <h2 className="text-xl font-bold mb-2">Get Best Price</h2>
-          <p>
-            Lorem ipsum dolor sit amet, <br /> consectetur adipiscing elit. Sed
-            <br /> arcu eu, dui tortor, proin eu lectus <br /> pellentesque. A
-            cras suscipit amet
-          </p>
-          <button className="bg-green-600 hover:bg-green-800 text-white font-bold py-2 px-4 rounded-2xl mt-4">
-            Read More
-          </button>
-        </div>
 
-        <div className="bg-white shadow-md p-4 rounded-lg">
-          <div>
-            <RiTreasureMapFill className="w-[40px] h-[40px]" />
-          </div>
-          <h2 className="text-xl font-bold mb-2">All Destination</h2>
-          <p>
-            Lorem ipsum dolor sit amet, <br /> consectetur adipiscing elit. Sed
-            <br /> arcu eu, dui tortor, proin eu lectus <br /> pellentesque. A
-            cras suscipit amet
-          </p>
-          <button className="bg-green-600 hover:bg-green-800 text-white font-bold py-2 px-4 rounded-2xl mt-4">
-            Read More
-          </button>
-        </div>
+        {services.map((service, index) => (
 
-        <div className="bg-white shadow-md p-4 rounded-lg">
-          <div>
-            <TfiMapAlt className="w-[40px] h-[40px]" />
-          </div>
-          <h2 className="text-xl font-bold mb-2">City Tours</h2>
-          <p>
-            Lorem ipsum dolor sit amet, <br /> consectetur adipiscing elit. Sed
-            <br /> arcu eu, dui tortor, proin eu lectus <br /> pellentesque. A
-            cras suscipit amet
-          </p>
-          <button className="bg-green-600 hover:bg-green-800 text-white font-bold py-2 px-4 rounded-2xl mt-4">
-            Read More
-          </button>
-        </div>
+          <div key={index} className="bg-white shadow-md p-4 rounded-lg">
 
-        <div className="bg-white shadow-md p-4 rounded-lg">
-          <div>
-            <MdOutlinePhotoCamera className="w-[40px] h-[40px]" />
+            {getIcon(service.icon)}
+            <h2 className="text-xl font-bold mb-2">{service.title}</h2>
+
+            <p>{service.description}</p>
+
+            <button className="bg-green-600 hover:bg-green-800 text-white font-bold py-2 px-4 rounded-2xl mt-4">
+              Read More
+            </button>
+
           </div>
-          <h2 className="text-xl font-bold mb-2">Photography</h2>
-          <p>
-            Lorem ipsum dolor sit amet, <br /> consectetur adipiscing elit. Sed
-            <br /> arcu eu, dui tortor, proin eu lectus <br /> pellentesque. A
-            cras suscipit amet
-          </p>
-          <button className="bg-green-600 hover:bg-green-800 text-white font-bold py-2 px-4 rounded-2xl mt-4">
-            Read More
-          </button>
-        </div>
+
+        ))}
+
       </div>
       <div className="flex items-center justify-between p-12">
-        <div className="relative bg-white shadow-md rounded-lg overflow-hidden">
-          <img
-            src={Img1}
-            alt="New Service 1"
-            className="w-full h-full object-cover rounded-lg"
-          />
-          <div className="absolute top-0 left-0 right-0 bottom-0 flex flex-col justify-start items-start p-4 bg-opacity-0 bg-black text-white">
-            <h1 className="text-3xl font-bold mb-2">Water City Dreams</h1>
-            <h2 className="text-xl font-bold">September 18, 2022</h2>
-            <p className="mt-2">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit ut
-              aliquam, purus sit amet luctus venenatis
-            </p>
-            <button className=" border border-green-400 hover:bg-green-800 text-white font-bold py-2 px-4 rounded-2xl mt-4">
-              Read More
-            </button>
+
+        {featuredServices.map((featured, index) => (
+
+          <div
+            key={index}
+            className="relative bg-white shadow-md rounded-lg overflow-hidden" >
+
+            <img  src={featured.image}  alt={featured.title} className="w-full h-full object-cover rounded-lg"/>
+
+            <div className="absolute top-0 left-0 right-0 bottom-0 flex flex-col justify-start items-start p-4 bg-opacity-0 bg-black text-white">
+
+              <h1 className="text-3xl font-bold mb-2">{featured.title}</h1>
+
+              <h2 className="text-xl font-bold">{featured.date}</h2>
+
+              <p className="mt-2">{featured.description}</p>
+
+              <button className="border border-green-400 hover:bg-green-800 text-white font-bold py-2 px-4 rounded-2xl mt-4">
+                {featured.buttonText}
+              </button>
+
+            </div>
           </div>
-        </div>
-        <div className="relative bg-white shadow-md rounded-lg overflow-hidden">
-          <img
-            src={Img2}
-            alt="New Service 1"
-            className="w-full h-full object-cover rounded-lg"
-          />
-          <div className="absolute top-0 left-0 right-0 bottom-0 flex flex-col justify-start items-start p-4 bg-opacity-0 bg-black text-white">
-            <h1 className="text-3xl font-bold mb-2">Hokkaido</h1>
-            <h2 className="text-xl font-bold">September 18, 2022</h2>
-            <p className="mt-2">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit ut
-              aliquam, purus sit amet luctus venenatis
-            </p>
-            <button className=" border border-green-400 hover:bg-green-800 text-white font-bold py-2 px-4 rounded-2xl mt-4">
-              Read More
-            </button>
-          </div>
-        </div>
-        <div className="relative bg-white shadow-md rounded-lg overflow-hidden">
-          <img
-            src={Img3}
-            alt="New Service 1"
-            className="w-full h-full object-cover rounded-lg"
-          />
-          <div className="absolute top-0 left-0 right-0 bottom-0 flex flex-col justify-start items-start p-4 bg-opacity-0 bg-black text-white">
-            <h1 className="text-3xl font-bold mb-2">Blue Lagoon</h1>
-            <h2 className="text-xl font-bold">September 18, 2022</h2>
-            <p className="mt-2">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit ut
-              aliquam, purus sit amet luctus venenatis
-            </p>
-            <button className=" border border-green-400 hover:bg-green-800 text-white font-bold py-2 px-4 rounded-2xl mt-4">
-              Read More
-            </button>
-          </div>
-        </div>
+
+        ))}
+
       </div>
+
       <div className="relative bg-white shadow-md rounded-lg overflow-hidden">
-        <img
-          src={Img4}
-          alt="Facilities"
-          className="w-full h-full object-cover "
-        />
+
+        <img src={facilities.image} alt={facilities.title} className="w-full h-full object-cover"  />
+
         <div className="absolute top-0 left-0 right-[50%] h-full flex flex-col justify-center items-start p-8 bg-opacity-0 bg-black text-white ">
-          <h1 className="text-4xl font-bold mb-2">Facilities</h1>
-          <p className="text-lg text-start">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit ut aliquam,
-            purus sit amet luctus venenatis, lectus magna fringilla urna,
-            porttitor rhoncus dolor purus non enim luctus venenatis, lectus
-            magna fringilla urna, porttitor rhoncus dolor purus non eni
-          </p>
+
+          <h1 className="text-4xl font-bold mb-2">{facilities.title}</h1>
+
+          <p className="text-lg text-start">{facilities.description}</p>
+
           <button className="bg-green-400 hover:bg-green-800 text-white font-bold py-2 px-4 rounded-md mt-4">
             Read More
           </button>
+
         </div>
+
         <div className="absolute top-0 left-[50%] w-[50%] h-full flex flex-col justify-center items-center p-4 bg-opacity-0 bg-black text-white text-start">
+
           <div className="grid grid-cols-2 grid-rows-2 gap-6 ">
-            <div className="bg-white text-black shadow-md p-4 rounded-lg cursor-pointer">
-              <FaBabyCarriage className=" w-10 h-10" />
 
-              <h2 className="text-3xl font-bold mb-2">Baby</h2>
-              <p>FREE BUGGY</p>
-            </div>
-            <div className="bg-white text-black shadow-md p-4 rounded-lg cursor-pointer">
-              <PiWineFill className=" w-10 h-10" />
-              <h2 className="text-3xl font-bold mb-2">Drinks</h2>
+            {additionalServices.map((additional, index) => (
+              <div  key={index} className="bg-white text-black shadow-md p-4 rounded-lg cursor-pointer" >
 
-              <p>INCLUDED</p>
-            </div>
-            <div className="bg-white text-black shadow-md p-4 rounded-lg cursor-pointer">
-              <FaCampground className=" w-10 h-10" />
-              <h2 className="text-3xl font-bold mb-2">Camp</h2>
+                {getIcon(additional.icon)}
 
-              <p>INCLUDED</p>
-            </div>
+                <h2 className="text-3xl font-bold mb-2">{additional.title}</h2>
 
-            <div className="bg-white text-black shadow-md p-4 rounded-lg cursor-pointer">
-              <LiaMapMarkedAltSolid className=" w-10 h-10" />
-              <h2 className="text-3xl font-bold mb-2">World</h2>
+                <p>{additional.description}</p>
 
-              <p>CUSTOM TOURS</p>
-            </div>
+              </div>
+
+            ))}
+
           </div>
         </div>
-      </div>{" "}
+
+      </div>
     </div>
   );
+};
+
+const getIcon = (iconName) => {
+  switch (iconName) {
+
+    case "ImPriceTags":
+      return <ImPriceTags className="w-[40px] h-[40px]" />;
+
+    case "RiTreasureMapFill":
+      return <RiTreasureMapFill className="w-[40px] h-[40px]" />;
+
+    case "TfiMapAlt":
+      return <TfiMapAlt className="w-[40px] h-[40px]" />;
+
+    case "MdOutlinePhotoCamera":
+      return <MdOutlinePhotoCamera className="w-[40px] h-[40px]" />;
+
+    case "FaBabyCarriage":
+      return <FaBabyCarriage className="w-10 h-10" />;
+
+    case "PiWineFill":
+      return <PiWineFill className="w-10 h-10" />;
+
+    case "FaCampground":
+      return <FaCampground className="w-10 h-10" />;
+
+    case "LiaMapMarkedAltSolid":
+      return <LiaMapMarkedAltSolid className="w-10 h-10" />;
+      
+    default:
+      return null;
+  }
 };
 
 export default ServiceHero;
